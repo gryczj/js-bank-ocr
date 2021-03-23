b0e7ba6be5bb3d7c521b9a7a090b592884900c89
Joanna Grycz <asia.grycz@gmail.com>
2021-03-23 17:48:54 +0100

    First commit

---
+++ src/reader.js
@@ -0,0 +1,8 @@
+module.exports = {
+    /**
+     * Thi is just a placeholder, implement however you like!
+     */
+    parse: (input)=>{
+        return "🤷‍♂️";
+    }
+}
---
+++ src/validator.js
@@ -0,0 +1,8 @@
+module.exports = {
+    /**
+     * Thi is just a placeholder, implement however you like!
+     */
+    validate: (input)=>{
+        return "🤷‍♂️";
+    }
+}
